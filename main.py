9ed71590521c94a8efdcda73c2ffa9f3afed2ceb
Shyaam-5 <shyaamkumar31@gmail.com>
2025-08-28 20:02:31 +0530

    3rd commit

--- main.py
+++ main.py
@@ -202,11 +202,4 @@ async def global_exception_handler(request, exc):
         ).dict()
     )
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app", 
-        host="0.0.0.0", 
-        port=8000, 
-        reload=True,
-        log_level="info"
-    )
+
